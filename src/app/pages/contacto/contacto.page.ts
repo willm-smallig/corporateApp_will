5c9361bf6d5d766bbe9dfde8495c1e14e7221590
manana <wellintonbg@mail.com>
2026-07-01 13:56:04 +0200

    Contacto y modelo

--- src/app/pages/contacto/contacto.page.ts
+++ src/app/pages/contacto/contacto.page.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButton, IonContent, IonHeader, IonItem, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.page.html',
   styleUrls: ['./contacto.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItem, IonButton]
 })
 export class ContactoPage implements OnInit {
+datosGuardados() {
+throw new Error('Method not implemented.');
+}
+correo: string = '';
+mensaje: string = '';
 
   constructor() { }
 
   ngOnInit() {
   }
-
+async enviar() {
+  await Preferences.set({
+key: 'ultimoMensaje',
+value: JSON.stringify({
+correo:this.correo,
+mensaje:this.mensaje
+})
+});
+}
 }
